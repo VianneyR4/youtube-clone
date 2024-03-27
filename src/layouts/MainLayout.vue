4e2317994bbd25a4a407bae7f2a9b625adebe4b2
VianneyR4 <vianneyrwicha2017@gmail.com>
2024-03-27 18:50:37 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,157 @@
+<template>
+  <q-layout view="hHh Lpr lff">
+    <!-- Header Starts -->
+    <q-header class="header">
+      <div class="header__left">
+        <i id="menu" class="material-icons" @click="toggleLeftDrawer">menu</i>
+        <img
+          src="../assets/cdnlogo.com_youtube-icon.svg"
+          alt="ytb"
+          height="25px"
+        />
+      </div>
+
+      <div class="header__search">
+        <form action="">
+          <input type="text" placeholder="Search" />
+          <button><i class="material-icons">search</i></button>
+        </form>
+      </div>
+
+      <div class="header__icons">
+        <i class="material-icons display-this">search</i>
+        <i class="material-icons">videocam</i>
+        <i class="material-icons">apps</i>
+        <i class="material-icons">notifications</i>
+        <i class="material-icons display-this">account_circle</i>
+      </div>
+    </q-header>
+    <!-- Header Ends -->
+
+    <!-- Sidebar Starts -->
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="100"
+      class="sidebar"
+    >
+      <!-- <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list> -->
+
+      <div class="sidebar__categories">
+        <div class="sidebar__category active">
+          <i class="material-icons">home</i>
+          <span>Home</span>
+        </div>
+        <div class="sidebar__category">
+          <i class="material-icons">local_fire_department</i>
+          <span>Trending</span>
+        </div>
+        <div class="sidebar__category">
+          <i class="material-icons">subscriptions</i>
+          <span>Subcriptions</span>
+        </div>
+      </div>
+      <hr />
+      <div class="sidebar__categories">
+        <div class="sidebar__category">
+          <i class="material-icons">library_add_check</i>
+          <span>Library</span>
+        </div>
+        <div class="sidebar__category">
+          <i class="material-icons">history</i>
+          <span>History</span>
+        </div>
+        <div class="sidebar__category">
+          <i class="material-icons">play_arrow</i>
+          <span>Your Videos</span>
+        </div>
+        <div class="sidebar__category">
+          <i class="material-icons">watch_later</i>
+          <span>Watch Later</span>
+        </div>
+        <div class="sidebar__category">
+          <i class="material-icons">thumb_up</i>
+          <span>Liked Videos</span>
+        </div>
+      </div>
+      <hr />
+    </q-drawer>
+    <!-- Sidebar Ends -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+defineOptions({
+  name: "MainLayout",
+});
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
+
+<style>
+@import "../css/style.css";
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,452 @@
+<template>
+  <q-page class="videos">
+    <div class="filter-container">
+      <div class="filter-scroll">
+        <p class="top-filter active">All</p>
+        <p class="top-filter">Music</p>
+        <p class="top-filter">Mix</p>
+        <p class="top-filter">Football</p>
+        <p class="top-filter">Real Madrid Footbal club</p>
+        <p class="top-filter">Apple</p>
+        <p class="top-filter">Kygo</p>
+        <p class="top-filter">Rhym and blue</p>
+        <p class="top-filter">In live</p>
+        <p class="top-filter">Music</p>
+        <p class="top-filter">Mix</p>
+        <p class="top-filter">Football</p>
+        <p class="top-filter">Real Madrid Footbal club</p>
+        <p class="top-filter">Apple</p>
+        <p class="top-filter">Kygo</p>
+        <p class="top-filter">Rhym and blue</p>
+        <p class="top-filter">In live</p>
+      </div>
+    </div>
+    <div class="videos__container">
+      <!-- Single Video starts -->
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax2.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Top 5 Programming Languages to Learn in 2021 | Best Programming
+              Languages to Learn
+            </h3>
+            <a href="">FutureCoders</a>
+            <span>10M Views • 3 Months Ago</span>
+          </div>
+        </div>
+      </div>
+      <!-- Single Video Ends -->
+
+      <!-- Single Video starts -->
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax3.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Build A Password Generator with React JS - Beginners Tutorial
+            </h3>
+            <a href="">FutureCoders</a>
+            <span>10M Views • 3 Months Ago</span>
+          </div>
+        </div>
+      </div>
+      <!-- Single Video Ends -->
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax3.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Bella Ciao Full Song | La Casa De Papel | Money Heist | Netflix
+              India
+            </h3>
+            <a href="">Netflix</a>
+            <span>10M Views • 11 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax2.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>DON'T EVER GIVE UP - Elon Musk (Motivational Video)</h3>
+            <a href=""> Chispa Motivation</a>
+            <span>10M Views • 1 Month Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Javascript Fundamentals</h3>
+            <a href="">Coding Addict</a>
+            <span>179K • 8 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax3.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Build 15 JavaScript Projects - Vanilla JavaScript Course</h3>
+            <a href=""> freeCodeCamp.org </a>
+            <span>470K Views • 8 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Build Real Time Face Detection With JavaScript</h3>
+            <a href=""> Web Dev Simplified </a>
+            <span>875K Views • 1 Year Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax3.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>AWS Basics for Beginners - Full Course</h3>
+            <a href=""> freeCodeCamp.org </a>
+            <span>36K Views • 1 Day Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <!-- Single Video starts -->
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax2.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Top 5 Programming Languages to Learn in 2021 | Best Programming
+              Languages to Learn
+            </h3>
+            <a href="">FutureCoders</a>
+            <span>10M Views • 3 Months Ago</span>
+          </div>
+        </div>
+      </div>
+      <!-- Single Video Ends -->
+
+      <!-- Single Video starts -->
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax3.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Build A Password Generator with React JS - Beginners Tutorial
+            </h3>
+            <a href="">FutureCoders</a>
+            <span>10M Views • 3 Months Ago</span>
+          </div>
+        </div>
+      </div>
+      <!-- Single Video Ends -->
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax3.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Bella Ciao Full Song | La Casa De Papel | Money Heist | Netflix
+              India
+            </h3>
+            <a href="">Netflix</a>
+            <span>10M Views • 11 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax2.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>DON'T EVER GIVE UP - Elon Musk (Motivational Video)</h3>
+            <a href=""> Chispa Motivation</a>
+            <span>10M Views • 1 Month Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Javascript Fundamentals</h3>
+            <a href="">Coding Addict</a>
+            <span>179K • 8 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax3.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Build 15 JavaScript Projects - Vanilla JavaScript Course</h3>
+            <a href=""> freeCodeCamp.org </a>
+            <span>470K Views • 8 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Build Real Time Face Detection With JavaScript</h3>
+            <a href=""> Web Dev Simplified </a>
+            <span>875K Views • 1 Year Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax3.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>AWS Basics for Beginners - Full Course</h3>
+            <a href=""> freeCodeCamp.org </a>
+            <span>36K Views • 1 Day Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <!-- Single Video starts -->
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax2.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Top 5 Programming Languages to Learn in 2021 | Best Programming
+              Languages to Learn
+            </h3>
+            <a href="">FutureCoders</a>
+            <span>10M Views • 3 Months Ago</span>
+          </div>
+        </div>
+      </div>
+      <!-- Single Video Ends -->
+
+      <!-- Single Video starts -->
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax3.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Build A Password Generator with React JS - Beginners Tutorial
+            </h3>
+            <a href="">FutureCoders</a>
+            <span>10M Views • 3 Months Ago</span>
+          </div>
+        </div>
+      </div>
+      <!-- Single Video Ends -->
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax3.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>
+              Bella Ciao Full Song | La Casa De Papel | Money Heist | Netflix
+              India
+            </h3>
+            <a href="">Netflix</a>
+            <span>10M Views • 11 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax2.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>DON'T EVER GIVE UP - Elon Musk (Motivational Video)</h3>
+            <a href=""> Chispa Motivation</a>
+            <span>10M Views • 1 Month Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Javascript Fundamentals</h3>
+            <a href="">Coding Addict</a>
+            <span>179K • 8 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax1.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax3.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Build 15 JavaScript Projects - Vanilla JavaScript Course</h3>
+            <a href=""> freeCodeCamp.org </a>
+            <span>470K Views • 8 Months Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax2.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>Build Real Time Face Detection With JavaScript</h3>
+            <a href=""> Web Dev Simplified </a>
+            <span>875K Views • 1 Year Ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="video">
+        <div class="video__thumbnail">
+          <img src="../assets/parallax3.jpg" alt="" />
+        </div>
+        <div class="video__details">
+          <div class="author">
+            <img src="../assets/parallax1.jpg" alt="" />
+          </div>
+          <div class="title">
+            <h3>AWS Basics for Beginners - Full Course</h3>
+            <a href=""> freeCodeCamp.org </a>
+            <span>36K Views • 1 Day Ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: "IndexPage",
+});
+</script>
